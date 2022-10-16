3553256bc495aaab75f5a07533bc00701ae0ac5b
Zabashta_Artur <zarpost@gmail.com>
2022-10-16 13:33:05 +0300

    feat: add solution of last task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {  
+  /*throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here*/
+  let listDeep = 0;
+  let valueArray = [];
+  let checkedValueArray = [];
+  function getDeep(list) {
+    
+    valueArray.push(list.value);
+    if (list.value !== k) checkedValueArray.push(list.value);
+
+    if (list.next)  {      
+      listDeep ++; 
+      getDeep(list.next)
+      } else return listDeep;
+  }
+  getDeep(l);  
+
+  checkedValueArray.forEach((elem, index) => {
+    switch (index) {
+      case 0: {
+        l.value = elem;        
+        if (index < checkedValueArray.length-1) l.next = {};
+        else l.next = null;
+        break;
+      }
+      case 1: {
+        l.next.value = elem;
+        if (index < checkedValueArray.length-1) l.next.next = {};
+        else l.next.next = null;
+        break;
+      }
+      case 2: {
+        l.next.next.value = elem;
+        if (index < checkedValueArray.length-1) l.next.next.next = {};
+        else l.next.next.next = null;
+        break;
+      }
+      case 3: {
+        l.next.next.next.value = elem;
+        if (index < checkedValueArray.length-1) l.next.next.next.next = {};
+        else l.next.next.next.next = null;
+        break;
+      }
+      case 4: {
+        l.next.next.next.next.value = elem;
+        if (index < checkedValueArray.length-1) l.next.next.next.next.next = {};
+        else l.next.next.next.next.next = null;
+        break;
+      }
+    }
+    
+  })
+
+  return l;
+
 }
 
 module.exports = {
